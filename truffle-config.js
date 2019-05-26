a6cc497a3a9c6cb53cb8d09ff130d19d887d3f56
KB109-linux <wangcser@qq.com>
2019-05-26 09:09:04 +0800

    finish the migrate work to private chain in truffle and ganache.

--- truffle-config.js
+++ truffle-config.js
@@ -4,7 +4,7 @@ module.exports = {
   // You can also follow this format for other networks;
   // see <http://truffleframework.com/docs/advanced/configuration>
   // for more details on how to specify configuration options!
-  /*
+  
   networks: {
     development: {
       host: "127.0.0.1",
@@ -16,19 +16,23 @@ module.exports = {
       port: 7545,
       network_id: "*"
     }
-  }
-  */
-  networks: {
-    development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*"
+  },
+
+  // coverage: {
+  //   host: 'localhost',
+  //   network_id: '*', // eslint-disable-line camelcase
+  //   port: 8555,
+  //   gas: 0xfffffffffff,
+  //   gasPrice: 0x01,
+  // },
+  
+  compilers: {
+    solc: {
+      version: "0.5.8",
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
     }
   }
-  
-  // compilers: {
-  //   solc: {
-  //     version: "0.5.8"
-  //   }
-  // }
 };
